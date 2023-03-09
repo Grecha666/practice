679c6c8a235de64d98455b3a46cf4fb062e02fc4
Anya Grechina <drivepie91@mail.ru>
2023-03-09 22:27:54 +0300

    шестая прога

---
+++ 006example/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username == "аня")
+{
+    Console.Write("Анюткин, приветик!");
+}
+else
+{
+    Console.Write("Тебе тут не рады, уходи");
+}
+

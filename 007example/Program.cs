039923af258cec2cba8cc7472531b1bacaadaec7
Anya Grechina <drivepie91@mail.ru>
2023-03-09 22:43:22 +0300

    седьмая прога

---
+++ 007example/Program.cs
@@ -0,0 +1,15 @@
+﻿int A = 1;
+int B = 2;
+int C = 6;
+int D = 8;
+int E = 4;
+
+int MAX = A;
+
+if (A > MAX) MAX = A;
+if (B > MAX) MAX = B;
+if (C > MAX) MAX = C;
+if (D > MAX) MAX = D;
+if (E > MAX) MAX = E;
+
+Console.WriteLine(MAX);
